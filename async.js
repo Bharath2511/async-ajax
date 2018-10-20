d7fa9f93b95f190a52aece0fc94cd8e1643881e2
V S Bharath <bharathcse5t6@gmail.com>
2018-10-20 12:33:01 +0530

    callback hell

--- async.js
+++ async.js
@@ -1,11 +1,32 @@
 //fires when window loads
 window.onload = function() {
 
-$.get('data/tweets.json',(data)=>{
-  console.log(data)
-})
-this.console.log('test')
+var handleError = (jqXHR,textStatus,error)=>{
 
+    console.log(error)
+}
+
+ $.ajax({
+     type : "GET",
+     url:"data/tweets.json",
+     success : cbTweets,
+     error :handleError
+ })
+ function cbTweets(data) {
+    console.log(data)
+
+    $.ajax({
+        type : "GET",
+        url:"data/friends.json",
+        success : (data)=>{
+           console.log(data)
+   
+           
+        },
+        error :handleError
+    })
+   
+ }
 
    
 
