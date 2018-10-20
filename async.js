1fbfd99b485753cadc51438138b0cdbd9a8acc63
V S Bharath <bharathcse5t6@gmail.com>
2018-10-20 13:04:25 +0530

    promises + jquery

--- async.js
+++ async.js
@@ -1,32 +1,41 @@
 //fires when window loads
 window.onload = function() {
 
-function get(url) {
-    return new Promise(function(resolve,reject){
-        var xhttp = new XMLHttpRequest()
-        xhttp.open('GET',url,true)
-        xhttp.onload = function() {
-            if(xhttp.status == 200) {
-                resolve(JSON.parse(xhttp.response))
-            }else{
-                reject(xhttp.statusText)
-            }
-        }
-        xhttp.onerror = function() {
-            reject(xhttp.statusText)
-        }
-        xhttp.send()
-    })
-}
+  $.get('data/tweets.json').then(function(tweets){
+      console.log(tweets)
+      return $.get('data/friends.json')
+  }).then((friends)=>{
+      console.log(friends)
+  }).catch((error)=>{
+      console.log(error)
+  })
 
-var promise = get('data/tweets.json')
-promise.then(function(tweets){
-    console.log(tweets)
-    return get('data/friends.json')
-}).then(function(friends){
-    console.log(friends)
-}).catch((error)=>{
-    console.log(error)
-})
+// function get(url) {
+//     return new Promise(function(resolve,reject){
+//         var xhttp = new XMLHttpRequest()
+//         xhttp.open('GET',url,true)
+//         xhttp.onload = function() {
+//             if(xhttp.status == 200) {
+//                 resolve(JSON.parse(xhttp.response))
+//             }else{
+//                 reject(xhttp.statusText)
+//             }
+//         }
+//         xhttp.onerror = function() {
+//             reject(xhttp.statusText)
+//         }
+//         xhttp.send()
+//     })
+// }
+
+// var promise = get('data/tweets.json')
+// promise.then(function(tweets){
+//     console.log(tweets)
+//     return get('data/friends.json')
+// }).then(function(friends){
+//     console.log(friends)
+// }).catch((error)=>{
+//     console.log(error)
+// })
 
 }
